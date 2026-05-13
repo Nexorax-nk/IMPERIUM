7068d37b3dee00c309dae4a3e2e10f1b0aef5ee1
ROSHAN-456 <roshanmagesh07@gmail.com>
2026-05-13 12:10:58 +0530

    src and dist

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ImperiumPage from './ImperiumPage'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ImperiumPage />
+  </React.StrictMode>,
+)
+
