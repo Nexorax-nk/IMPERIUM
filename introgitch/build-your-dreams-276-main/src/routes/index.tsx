b6f7935e34ad5a97014ea6606acdc1ee5d3074bd
Nimalan Mani <mnimalanmani@gmail.com>
2026-05-16 13:41:55 +0530

    Added dashboard, updated schemas and UI

--- introgitch/build-your-dreams-276-main/src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import ImperiumIntro from "@/components/ImperiumIntro";
-
-export const Route = createFileRoute("/")({
-  component: ImperiumIntro,
-});
